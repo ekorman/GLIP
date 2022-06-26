f35bfe0272a8b67e3843956cf21e2ca3f85a8d9d
Eric O. Korman <eric.korman@gmail.com>
2022-06-26 13:47:46 -0500

    get rid of losses

--- maskrcnn_benchmark/modeling/rpn/fcos.py
+++ maskrcnn_benchmark/modeling/rpn/fcos.py
@@ -5,7 +5,6 @@ from torch import nn
 
 from maskrcnn_benchmark.modeling import registry
 from maskrcnn_benchmark.layers import Scale, DFConv2d
-from .loss import make_fcos_loss_evaluator
 from .anchor_generator import make_center_anchor_generator
 from .inference import make_fcos_postprocessor
 
@@ -28,8 +27,7 @@ class FCOSHead(torch.nn.Module):
         cls_tower = []
         bbox_tower = []
         for i in range(cfg.MODEL.FCOS.NUM_CONVS):
-            if use_dcn_in_tower and \
-                    i == cfg.MODEL.FCOS.NUM_CONVS - 1:
+            if use_dcn_in_tower and i == cfg.MODEL.FCOS.NUM_CONVS - 1:
                 conv_func = DFConv2d
             else:
                 conv_func = nn.Conv2d
@@ -41,7 +39,7 @@ class FCOSHead(torch.nn.Module):
                     kernel_size=3,
                     stride=1,
                     padding=1,
-                    bias=True
+                    bias=True,
                 )
             )
             if use_gn:
@@ -57,7 +55,7 @@ class FCOSHead(torch.nn.Module):
                     kernel_size=3,
                     stride=1,
                     padding=1,
-                    bias=True
+                    bias=True,
                 )
             )
             if use_gn:
@@ -66,25 +64,22 @@ class FCOSHead(torch.nn.Module):
                 bbox_tower.append(nn.BatchNorm2d(in_channels))
             bbox_tower.append(nn.ReLU())
 
-        self.add_module('cls_tower', nn.Sequential(*cls_tower))
-        self.add_module('bbox_tower', nn.Sequential(*bbox_tower))
+        self.add_module("cls_tower", nn.Sequential(*cls_tower))
+        self.add_module("bbox_tower", nn.Sequential(*bbox_tower))
         self.cls_logits = nn.Conv2d(
-            in_channels, num_classes, kernel_size=3, stride=1,
-            padding=1
+            in_channels, num_classes, kernel_size=3, stride=1, padding=1
         )
-        self.bbox_pred = nn.Conv2d(
-            in_channels, 4, kernel_size=3, stride=1,
-            padding=1
-        )
-        self.centerness = nn.Conv2d(
-            in_channels, 1, kernel_size=3, stride=1,
-            padding=1
-        )
+        self.bbox_pred = nn.Conv2d(in_channels, 4, kernel_size=3, stride=1, padding=1)
+        self.centerness = nn.Conv2d(in_channels, 1, kernel_size=3, stride=1, padding=1)
 
         # initialization
-        for modules in [self.cls_tower, self.bbox_tower,
-                        self.cls_logits, self.bbox_pred,
-                        self.centerness]:
+        for modules in [
+            self.cls_tower,
+            self.bbox_tower,
+            self.cls_logits,
+            self.bbox_pred,
+            self.centerness,
+        ]:
             for l in modules.modules():
                 if isinstance(l, nn.Conv2d):
                     torch.nn.init.normal_(l.weight, std=0.01)
@@ -137,17 +132,13 @@ class FCOSModule(torch.nn.Module):
         box_selector_train = make_fcos_postprocessor(cfg, is_train=True)
         box_selector_test = make_fcos_postprocessor(cfg, is_train=False)
 
-        loss_evaluator = make_fcos_loss_evaluator(cfg)
-
         self.cfg = cfg
         self.head = head
         self.box_selector_train = box_selector_train
         self.box_selector_test = box_selector_test
-        self.loss_evaluator = loss_evaluator
         self.fpn_strides = cfg.MODEL.FCOS.FPN_STRIDES
         if not cfg.MODEL.RPN_ONLY:
             self.anchor_generator = make_center_anchor_generator(cfg)
-
 
     def forward(self, images, features, targets=None):
         """
@@ -166,40 +157,16 @@ class FCOSModule(torch.nn.Module):
         """
         box_cls, box_regression, centerness = self.head(features)
         locations = self.compute_locations(features)
-        if self.training and targets is not None:
-            return self._forward_train(
-                locations, box_cls, box_regression,
-                centerness, targets, images.image_sizes
-            )
-        else:
-            return self._forward_test(
-                locations, box_cls, box_regression,
-                centerness, images.image_sizes
-            )
 
-    def _forward_train(self, locations, box_cls, box_regression, centerness, targets, image_sizes=None):
-        loss_box_cls, loss_box_reg, loss_centerness = self.loss_evaluator(
-            locations, box_cls, box_regression, centerness, targets
+        return self._forward_test(
+            locations, box_cls, box_regression, centerness, images.image_sizes
         )
-        losses = {
-            "loss_cls": loss_box_cls,
-            "loss_reg": loss_box_reg,
-            "loss_centerness": loss_centerness
-        }
-        if self.cfg.MODEL.RPN_ONLY:
-            return None, losses
-        else:
-            boxes = self.box_selector_train(
-                locations, box_cls, box_regression,
-                centerness, image_sizes
-            )
-            proposals = self.anchor_generator(boxes, image_sizes, centerness)
-            return proposals, losses
 
-    def _forward_test(self, locations, box_cls, box_regression, centerness, image_sizes):
+    def _forward_test(
+        self, locations, box_cls, box_regression, centerness, image_sizes
+    ):
         boxes = self.box_selector_test(
-            locations, box_cls, box_regression,
-            centerness, image_sizes
+            locations, box_cls, box_regression, centerness, image_sizes
         )
         if not self.cfg.MODEL.RPN_ONLY:
             boxes = self.anchor_generator(boxes, image_sizes, centerness)
@@ -210,27 +177,20 @@ class FCOSModule(torch.nn.Module):
         for level, feature in enumerate(features):
             h, w = feature.size()[-2:]
             locations_per_level = self.compute_locations_per_level(
-                h, w, self.fpn_strides[level],
-                feature.device
+                h, w, self.fpn_strides[level], feature.device
             )
             locations.append(locations_per_level)
         return locations
 
     def compute_locations_per_level(self, h, w, stride, device):
         shifts_x = torch.arange(
-            0, w * stride, step=stride,
-            dtype=torch.float32, device=device
+            0, w * stride, step=stride, dtype=torch.float32, device=device
         )
         shifts_y = torch.arange(
-            0, h * stride, step=stride,
-            dtype=torch.float32, device=device
+            0, h * stride, step=stride, dtype=torch.float32, device=device
         )
         shift_y, shift_x = torch.meshgrid(shifts_y, shifts_x)
         shift_x = shift_x.reshape(-1)
         shift_y = shift_y.reshape(-1)
         locations = torch.stack((shift_x, shift_y), dim=1) + stride // 2
         return locations
-
-
-
-

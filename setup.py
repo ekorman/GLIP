da7a59569c1005b30328a032b46e727e079c7e71
Eric O. Korman <eric.korman@gmail.com>
2022-09-05 15:19:33 -0500

    set torchvision version

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     install_requires=[
         "numpy",
         "torch==1.9.0",
-        "torchvision",
+        "torchvision==0.10.0",
         "einops",
         "timm",
         "yacs",
